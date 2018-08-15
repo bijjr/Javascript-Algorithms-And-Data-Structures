09f5f3ed8e2cc14ec39093d28b0e0b916ae0abca
bijjr <billiejefferys@gmail.com>
2018-08-14 21:21:29 -0400

    Understanding Uninitialized Variables

---
+++ Basic Javascript/main.js
@@ -0,0 +1,36 @@
+// Basic Javascript
+
+// var ourName;
+//
+// ourName = "Hello world";
+//
+//
+// var str = "Hello World!";
+// var n = str.length;
+//
+// console.log("the length of var ourName is:");
+// console.log(ourName.length);
+// console.log("the length of var str is:");
+// console.log(str.length);
+// console.log("the length of var n is:");
+// console.log(n.length);
+
+// Basic JavaScript: Understanding Uninitialized Variables
+
+// Initialize these three variables
+var a;
+var a = 5;
+var b;
+var b = 10;
+var c;
+var c = "I am a"
+
+// Do not change code below this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+
+console.log(a);
+console.log(b);
+console.log(c);
