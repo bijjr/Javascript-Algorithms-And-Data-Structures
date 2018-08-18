b47ba6e475e6ef12e3d5bf78cb37bb0b71a25aa2
bijjr <billiejefferys@gmail.com>
2018-08-17 23:29:10 -0400

    Javascript Objects

---
+++ Basic Javascript/core.js
@@ -0,0 +1,128 @@
+//Dot Notation
+
+var myObj = {
+  prop1: "val1",
+  prop2: "val2"
+};
+
+var prop1val = myObj.prop1; // val1
+var prop2val = myObj.prop2; // val2
+
+// Bracket Notation
+
+var myObj = {
+  "Space Name": "Kirk",
+  "More Space": "Spock",
+  "NoSpace": "USS Enterprise"
+};
+myObj["Space Name"]; // Kirk
+myObj['More Space']; // Spock
+myObj["NoSpace"]; // USS Enterprise
+
+// Accessing Object Properties with Variables
+
+// Setup
+var testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line;
+
+var playerNumber = 16;       // Change this Line
+var player = testObj[playerNumber];   // Change this Line
+
+// Updating Object Properties
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+ourDog.name = "Happy Camper";
+
+// Setup
+var myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line.
+
+myDog.name = "Happy Coder";
+myDog['name'] = "Happy Coder";
+
+// Setup
+function phoneticLookup(val) {
+  var result = "";
+
+  // Only change code below this line
+  var lookup = {
+    "alpha":"Adams",
+    "bravo":"Boston",
+    "charlie":"Chicago",
+    "delta":"Denver",
+    "echo":"Easy",
+    "foxtrot":"Frank"
+  }
+  result = lookup[val];
+  // Only change code above this line
+  return result;
+}
+
+// Change this value to test
+phoneticLookup["charlie"];
+
+
+// Setup
+var myObj = {
+  gift: "pony",
+  pet: "kitten",
+  bed: "sleigh"
+};
+
+function checkObj(checkProp) {
+  // Your Code Here
+  if (myObj.hasOwnProperty(checkProp)) {
+    return myObj[checkProp];
+  }
+  else {
+    return "Not Found";
+  }
+}
+
+// Test your code by modifying these values
+console.log(checkObj("gift"));
+
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+    'artist':'Drake',
+    'title':'Scorpion',
+    'release_year':'2018',
+    'formats': [
+      'CD',
+      'VINYL',
+      'Digital'
+    ],
+    'gold': true
+  }
+];
