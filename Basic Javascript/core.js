2ed5e4ab4c42f7fc31841d168ca3b5a9df9fe58f
bijjr <billiejefferys@gmail.com>
2018-08-17 23:39:37 -0400

    Arrays, Objects, etc

--- Basic Javascript/core.js
+++ Basic Javascript/core.js
@@ -126,3 +126,18 @@ var myMusic = [
     'gold': true
   }
 ];
+
+// Setup
+var myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+var gloveBoxContents = myStorage.car.inside['glove box']; // Change this line
