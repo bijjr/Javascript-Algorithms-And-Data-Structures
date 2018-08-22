e6d0ff4c7b301f7fed37cabf317c88b54f395784
bijjr <billiejefferys@gmail.com>
2018-08-22 00:07:28 -0400

    'let' Keywords

---
+++ ES6/es6.js
@@ -0,0 +1,11 @@
+// Explore Differences Between the var and let Keywords
+
+let catName;
+let quote;
+function catTalk() {
+  "use strict";
+  catName = "Oliver";
+  quote = catName + " says Meow!";
+  return quote
+}
+console.log(catTalk());
