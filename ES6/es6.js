7f550172bed2fbd1fc8e1db8ef23aeea2601efaa
bijjr <billiejefferys@gmail.com>
2018-08-22 07:49:10 -0400

    Understanding random ranges

--- ES6/es6.js
+++ ES6/es6.js
@@ -9,3 +9,23 @@ function catTalk() {
   return quote
 }
 console.log(catTalk());
+
+
+// console.log(Math.floor(Math.random() * ((5-3 + 1)+3)));
+
+function ourRandomRange(ourMin, ourMax) {
+
+  return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
+}
+
+// console.log(ourRandomRange(1, 9));
+
+function randomRange(min,max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(randomRange(3,5));
+
+console.log(Math.floor(Math.random() * ((5-3 + 1)+3)));
+
+console.log(Math.floor(Math.random() * (5 - 3 + 1)) + 3);
