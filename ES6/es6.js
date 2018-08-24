355aed2e89cfb6ad60aeffca5379816fe8a94b58
bijjr <billiejefferys@gmail.com>
2018-08-24 08:33:51 -0400

    Refactoring to arrow function

--- ES6/es6.js
+++ ES6/es6.js
@@ -100,11 +100,15 @@ console.log(squaredIntegers);
 
 // Default parameters for Functions
 
-const increment = (function() {
-  "use strict";
-  return function increment(number, value=1) {
-    return number + value;
-  };
-})();
+// const increment = (function() {
+//   "use strict";
+//   return function increment(number, value=1) {
+//     return number + value;
+//   };
+// })();
+
+const increment = (number, value = 1) => {
+  return number + value;
+};
 console.log(increment(5, 2)); // returns 7
 console.log(increment(5)); // returns NaN
