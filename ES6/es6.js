8a9d21b44456264b9973a3a24e207caf14fb80cc
bijjr <billiejefferys@gmail.com>
2018-08-24 08:22:07 -0400

    Default parameters for Functions

--- ES6/es6.js
+++ ES6/es6.js
@@ -96,3 +96,15 @@ const squareList = (arr) => {
 // test your code
 const squaredIntegers = squareList(realNumberArray);
 console.log(squaredIntegers);
+
+
+// Default parameters for Functions
+
+const increment = (function() {
+  "use strict";
+  return function increment(number, value=1) {
+    return number + value;
+  };
+})();
+console.log(increment(5, 2)); // returns 7
+console.log(increment(5)); // returns NaN
