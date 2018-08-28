b30f118e7df610799fa9d32b2d268823058cb06f
bijjr <billiejefferys@gmail.com>
2018-08-27 22:36:43 -0400

    Template literals

--- ES6/es6.js
+++ ES6/es6.js
@@ -254,8 +254,37 @@ console.log(half(stats)); // should be 28.015
 
 
 //Topic
-//Prompt
+//Create Strings using Template Literals
 
+//Prompt
+//Use template literal syntax with backticks to display each entry of the result object's failure array. Each entry should be wrapped inside an li element with the class attribute text-warning, and listed within the resultDisplayArray.
+
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["id-blacklist", "no-dup-keys"]
+};
+function makeList(arr) {
+  "use strict";
+
+  // change code below this line
+  const resultDisplayArray = [
+    `<li class="text-warning">${arr[0]}</li>`,
+    `<li class="text-warning">${arr[1]}</li>`,
+    `<li class="text-warning">${arr[2]}</li>`
+  ];
+  // change code above this line
+
+  return resultDisplayArray;
+}
+/**
+ * makeList(result.failure) should return:
+ * [ `<li class="text-warning">no-var</li>`,
+ *   `<li class="text-warning">var-on-top</li>`,
+ *   `<li class="text-warning">linebreak</li>` ]
+ **/
+const resultDisplayArray = makeList(result.failure);
+console.log(resultDisplayArray);
 //Topic
 //Prompt
 
