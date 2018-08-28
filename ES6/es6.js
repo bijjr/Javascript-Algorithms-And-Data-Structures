680740df98b90e687a1596c57ca4a006b52bddc6
bijjr <billiejefferys@gmail.com>
2018-08-27 23:10:51 -0400

    Object Literals

--- ES6/es6.js
+++ ES6/es6.js
@@ -285,6 +285,26 @@ function makeList(arr) {
  **/
 const resultDisplayArray = makeList(result.failure);
 console.log(resultDisplayArray);
+
+//Topic
+//Object Literals
+
+//Prompt
+//Use simple fields with object literals to create and return a Person object.
+
+
+const createPerson = (name, age, gender) => {
+  "use strict";
+  // change code below this line
+  return {
+    name,
+    age,
+    gender
+  };
+  // change code above this line
+};
+console.log(createPerson("Zodiac Hasbro", 56, "male")); // returns a proper object
+
 //Topic
 //Prompt
 
