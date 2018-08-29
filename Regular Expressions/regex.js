821363c6fcd81d3806d844d56229dd2b31c59832
bijjr <billiejefferys@gmail.com>
2018-08-28 21:56:41 -0400

    OR operator

---
+++ Regular Expressions/regex.js
@@ -0,0 +1,150 @@
+//Topic
+//
+
+//Prompt
+//
+
+let myString = "Hello, World!";
+let myRegex = /Hello/;
+let result = myRegex.test(myString); // Change this line
+console.log(result);
+
+// setting a string variable
+let myStrings = "Hello";
+// setting patter for regex to match
+let myRegexes = /Hell/;
+// using test function to find patterns in string
+console.log(myRegexes.test(myStrings));
+console.log('============');
+
+//Topic
+//Match literal string with diff possibilities 
+// Prompt
+// Match regex to multiple patterns using OR Operators
+
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let resultOne = petRegex.test(petString);
+console.log(resultOne);
+
+console.log('============');
+
+//Topic
+//
+//Prompt
+//
+
+
+// console.log('============');
+
+//Topic
+//
+//Prompt
+//
+
+
+// console.log('============');
+
+//Topic
+//
+//Prompt
+//
+
+
+// console.log('============');
+
+//Topic
+//
+//Prompt
+//
+
+
+// console.log('============');
+
+//Topic
+//
+//Prompt
+//
+
+
+// console.log('============');
+
+//Topic
+//
+//Prompt
+//
+
+
+// console.log('============');
+//
+// //Topic
+// //
+// //Prompt
+// //
+//
+//
+// console.log('============');
+//
+// //Topic
+// //
+// //Prompt
+// //
+//
+//
+// console.log('============');
+//
+// //Topic
+// //
+// //Prompt
+// //
+//
+//
+// console.log('============');
+//
+// //Topic
+// //
+// //Prompt
+// //
+//
+//
+// console.log('============');
+//
+// //Topic
+// //
+// //Prompt
+// //
+//
+//
+// console.log('============');
+//
+// //Topic
+// //
+// //Prompt
+// //
+//
+//
+// console.log('============');
+//
+// //Topic
+// //
+// //Prompt
+// //
+//
+//
+// console.log('============');
+//
+// //Topic
+// //
+// //Prompt
+// //
+//
+//
+// console.log('============');
+//
+// //Topic
+// //
+// //Prompt
+// //
+//
+//
+// console.log('============');
