b2feaf2859f7fa5c668f68850a60c6fbf6d9a6d4
bijjr <billiejefferys@gmail.com>
2018-09-02 00:00:37 -0400

    reverse a string

---
+++ Javascript Algorithms/basic_algorithms.js
@@ -0,0 +1,73 @@
+//Convert Celsius to Fahrenheit
+
+function convertToF(celsius) {
+  let fahrenheit = (celsius * 9)/5 + 32;
+  return fahrenheit;
+}
+
+// console.log(convertToF(30));
+
+convertToF(0) //should return a number
+convertToF(-30) //should return a value of -22
+convertToF(-10) //should return a value of 14
+convertToF(0) //should return a value of 32
+convertToF(20) //should return a value of 68
+convertToF(30) //should return a value of 86
+
+console.log('====================');
+
+//Reverse a String
+
+function reverseString(str) {
+  return str;
+}
+
+reverseString("hello");
+
+// console.log('====================');
+
+//
+
+// console.log('====================');
+
+//
+
+// console.log('====================');
+
+//
+
+// console.log('====================');
+
+//
+
+// console.log('====================');
+
+//
+
+// console.log('====================');
+
+//
+
+// console.log('====================');
+
+//
+
+// console.log('====================');
+
+//
+
+// console.log('====================');
+
+//
+
+// console.log('====================');
+
+//
+
+// console.log('====================');
+
+//
+
+// console.log('====================');
+
+//
