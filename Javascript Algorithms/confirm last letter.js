faab8e70731d33f364d0ac6160155d1425fa5c3b
bijjr <billiejefferys@gmail.com>
2018-09-02 23:56:14 -0400

    placeholder for matchign endings

---
+++ Javascript Algorithms/confirm last letter.js
@@ -0,0 +1,12 @@
+//Check if a string (first argument, str) ends with the given target string (second argument, target).
+
+function confirmEnding(str, target) {
+  let search = str.split('');
+  if last string == target
+    true
+  false
+  return search
+  }
+
+
+console.log(confirmEnding("Bastian", "n"));
