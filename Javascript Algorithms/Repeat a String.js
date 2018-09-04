c0f9e05a367d25f8ec1ce6f60e4a1b362cf89e20
bijjr <billiejefferys@gmail.com>
2018-09-03 23:01:20 -0400

    intial solution using iteration

--- Javascript Algorithms/Repeat a String.js
+++ Javascript Algorithms/Repeat a String.js
@@ -1,9 +1,30 @@
 //Repeat a given string str (first argument) for num times (second argument). Return an empty string if num is not a positive number.
 
-
+// initial solution
 function repeatStringNumTimes(str, num) {
-  
-  return str;
+  let newstring = '';
+
+  if (num > 0) {
+    for (var i = 0; i < num; i++) {
+      newstring += str;
+    }
+  }
+  return newstring
 }
 
-repeatStringNumTimes("abc", 3);
+//
+
+
+
+console.log(repeatStringNumTimes("*", 3));
+// should return "***".
+console.log(repeatStringNumTimes("abc", 3));
+// should return "abcabcabc".
+console.log(repeatStringNumTimes("abc", 4));
+// should return "abcabcabcabc".
+console.log(repeatStringNumTimes("abc", 1));
+// should return "abc".
+console.log(repeatStringNumTimes("*", 8));
+// should return "********".
+console.log(repeatStringNumTimes("abc", -2));
+// should return "".
