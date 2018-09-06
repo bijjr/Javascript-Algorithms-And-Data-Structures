1e1d024cb15b68af89b0296f1fc8a99f45b92e2b
bijjr <billiejefferys@gmail.com>
2018-09-05 23:24:01 -0400

    Initial solution using ternary operators

--- Javascript Algorithms/boo who.js
+++ Javascript Algorithms/boo who.js
@@ -3,10 +3,32 @@
 
 // Initial solution
 function booWho(bool) {
-
-  return bool;
+  return typeof bool == typeof true || false ? true : false;
+  // bool == true ? true : bool == false ? true : false ;
 }
 
-booWho(null);
+console.log(booWho(null));
+// should return false.
+console.log(booWho(true));
+// should return true.
+console.log(booWho(false));
+// should return true.
+console.log(booWho([1, 2, 3]));
+// should return false.
+console.log(booWho([].slice));
+// should return false.
+console.log(booWho({ "a": 1 }));
+// should return false.
+console.log(booWho(1));
+// should return false.
+console.log(booWho(NaN));
+// should return false.
+console.log(booWho("a"));
+// should return false.
+console.log(booWho("true"));
+// should return false.
+console.log(booWho("false"));
+// should return false.
+
 
 // Optimal solution
