52b39eda9fa261c3eed392c3392a81bd7e893f8a
bijjr <billiejefferys@gmail.com>
2018-09-06 00:08:55 -0400

    Added Advanced Solution for "Title Case" exercise

--- Javascript Algorithms/title case.js
+++ Javascript Algorithms/title case.js
@@ -6,12 +6,19 @@
 
 // intial solution
 function titleCase(str) {
-  return str.split(' ')
-  .map(i => {
-    return i.charAt(0).toUpperCase() +
-    i.substr(1).toLowerCase()
-  })
-  .join(' ');
+  return str.split(' ').map(i => {
+    return i.charAt(0).toUpperCase() + i.substr(1).toLowerCase()
+  }).join(' ');
+}
+
+//advanced solution
+function titleCase(str) {
+  // convert entire string to toLowerCase
+  //replaces first letter with uppercase
+  // regex -- (^ at the beginning of string OR anywhitespace) followed by non whitespace in the entire string
+  // .replace takes 2 arguements, 1 being the regex ^^^^
+  // the second being an imeediately incoked function expression changing all patterns that meet regex to upper case
+  return str.toLowerCase().replace(/(^|\s)\S/g, (L) => L.toUpperCase());
 }
 
 
