4b4da16f453559f48a224f008a9191814dd95abf
bijjr <billiejefferys@gmail.com>
2018-09-05 23:35:21 -0400

    Adding "Title Case" exercise

---
+++ Javascript Algorithms/title case.js
@@ -0,0 +1,13 @@
+//Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+
+// Capitalize connecting words like "the" and "of".
+
+
+
+// intial solution
+function titleCase(str) {
+  let arr = str.split('')
+  let str = arr.map(letter => )
+}
+
+titleCase("I'm a little tea pot");
