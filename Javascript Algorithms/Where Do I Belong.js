149b2804509438947aad03f192811072140a0d7f
bijjr <billiejefferys@gmail.com>
2018-09-06 03:14:19 -0400

    Added "Where do i belong" exercise

---
+++ Javascript Algorithms/Where Do I Belong.js
@@ -0,0 +1,41 @@
+//Return the lowest index at which a value (second argument) should be inserted into an array (first argument) once it has been sorted. The returned value should be a number.
+
+function getIndexToIns(arr, num) {
+  
+  return num;
+}
+
+console.log(getIndexToIns([40, 60], 50));
+
+console.log(getIndexToIns([10, 20, 30, 40, 50], 35) );
+should return 3.
+console.log(getIndexToIns([10, 20, 30, 40, 50], 35));
+ should return a number.
+console.log(getIndexToIns([10, 20, 30, 40, 50], 30));
+ should return 2.
+console.log(getIndexToIns([10, 20, 30, 40, 50], 30) );
+should return a number.
+console.log(getIndexToIns([40, 60], 50) );
+should return 1.
+console.log(getIndexToIns([40, 60], 50) );
+should return a number.
+console.log(getIndexToIns([3, 10, 5], 3) );
+should return 0.
+console.log(getIndexToIns([3, 10, 5], 3) );
+should return a number.
+console.log(getIndexToIns([5, 3, 20, 3], 5));
+ should return 2.
+console.log(getIndexToIns([5, 3, 20, 3], 5));
+ should return a number.
+console.log(getIndexToIns([2, 20, 10], 19));
+ should return 2.
+console.log(getIndexToIns([2, 20, 10], 19));
+ should return a number.
+console.log(getIndexToIns([2, 5, 10], 15));
+ should return 3.
+console.log(getIndexToIns([2, 5, 10], 15));
+ should return a number.
+console.log(getIndexToIns([], 1));
+ should return 0.
+console.log(etIndexToIns([], 1));
+should return a number.
