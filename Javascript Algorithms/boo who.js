6104e93da7e8c7aaf7d8a33e40b86e8b5e1488ca
bijjr <billiejefferys@gmail.com>
2018-09-05 22:26:23 -0400

    Initial solution commit

---
+++ Javascript Algorithms/boo who.js
@@ -0,0 +1,12 @@
+//Check if a value is classified as a boolean primitive. Return true or false.
+
+
+// Initial solution
+function booWho(bool) {
+
+  return bool;
+}
+
+booWho(null);
+
+// Optimal solution
