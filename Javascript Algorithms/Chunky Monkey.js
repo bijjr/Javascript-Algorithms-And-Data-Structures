bb5fa8e4d967100391a4da7eb7d677e78c088cd2
bijjr <billiejefferys@gmail.com>
2018-09-07 01:34:52 -0400

    Added optimal solution for Chunky Monkey

--- Javascript Algorithms/Chunky Monkey.js
+++ Javascript Algorithms/Chunky Monkey.js
@@ -1,24 +1,52 @@
 //Write a function that splits an array (first argument) into groups the length of size (second argument) and returns them as a two-dimensional array.
 
+// initial solution
 function chunkArrayInGroups(arr, size) {
+  let chunks = [];
 
-  return arr;
+  while (arr.length > size) {
+      chunks.push(arr.splice(0, size));
+    }
+
+  (arr.length <= size) ? chunks.push(arr.splice(0)) : false ;
+
+  return chunks
 }
 
-chunkArrayInGroups(["a", "b", "c", "d"], 2);
+//optimal solutions
+chunkArrayInGroups(arr, size) {
+  let chunks = [];
+
+  while (arr.length ) {
+      chunks.push(arr.splice(0, size));
+    }
+
+  return chunks
+}
+
+
+// function chunkArrayInGroups(arr, size) {
+//   let chunks = [];
+//
+//    (arr.length > size) ? (chunks.push(arr.splice(0,size))) : (arr.length <= size) ? (chunks.push(arr.splice(0))) : false;
+//
+//   return chunks
+// }
+
+// chunkArrayInGroups(["a", "b", "c", "d"], 2);
 
 
 console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2) );
-// should return [["a", "b"], ["c", "d"]].
+console.log([["a", "b"], ["c", "d"]]);
 console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 3) );
-// should return [[0, 1, 2], [3, 4, 5]].
+console.log([[0, 1, 2], [3, 4, 5]]);
 console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 2) );
-// should return [[0, 1], [2, 3], [4, 5]].
+console.log([[0, 1], [2, 3], [4, 5]]);
 console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5], 4) );
-// should return [[0, 1, 2, 3], [4, 5]].
+console.log([[0, 1, 2, 3], [4, 5]]);
 console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6], 3) );
-// should return [[0, 1, 2], [3, 4, 5], [6]].
+console.log([[0, 1, 2], [3, 4, 5], [6]]);
 console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 4));
-// should return [[0, 1, 2, 3], [4, 5, 6, 7], [8]].
+console.log([[0, 1, 2, 3], [4, 5, 6, 7], [8]]);
 console.log(chunkArrayInGroups([0, 1, 2, 3, 4, 5, 6, 7, 8], 2));
-// should return [[0, 1], [2, 3], [4, 5], [6, 7], [8]].
+console.log([[0, 1], [2, 3], [4, 5], [6, 7], [8]]);
