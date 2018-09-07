b0ca3632e7eca82b1c314c354c94d8a11ef6ea8a
bijjr <billiejefferys@gmail.com>
2018-09-06 23:54:34 -0400

    Added initial "Mutations" exercise

--- Javascript Algorithms/Mutations.js
+++ Javascript Algorithms/Mutations.js
@@ -1,8 +1,36 @@
 //Return true if the string in the first element of the array contains all of the letters of the string in the second element of the array.
 
+
+//Initial solution
 function mutation(arr) {
-  
-  return arr;
+  //using .map() to lowercase all letters in the array
+  //using .split() to create arrays of each letter within each array
+  let string = arr.map(i => i.toLowerCase().split(''))
+  // return string[0].indexOf('k')
+  for (var i = 0; i < string[1].length; i++) {
+    if (string[0].indexOf(string[1][i]) === -1) {
+         return false
+    }
+}
+return true
 }
 
-mutation(["hello", "hey"]);
+
+console.log(mutation(["hello", "hey"]));
+// should return false.
+console.log(mutation(["hello", "Hello"]));
+ // should return true.
+console.log(mutation(["zyxwvutsrqponmlkjihgfedcba", "qrstu"]) );
+// should return true.
+console.log(mutation(["Mary", "Army"]) );
+// should return true.
+console.log(mutation(["Mary", "Aarmy"]) );
+// should return true.
+console.log(mutation(["Alien", "line"]) );
+// should return true.
+console.log(mutation(["floor", "for"]) );
+// should return true.
+console.log(mutation(["hello", "neo"]) );
+// should return false.
+console.log(mutation(["voodoo", "no"]) );
+// should return false.)
