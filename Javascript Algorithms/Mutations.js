cab31690a540be2b360729738cb9c1572b37f66e
bijjr <billiejefferys@gmail.com>
2018-09-07 00:16:33 -0400

    Added Optimal Solution for Mutations exercise

--- Javascript Algorithms/Mutations.js
+++ Javascript Algorithms/Mutations.js
@@ -2,17 +2,27 @@
 
 
 //Initial solution
+// function mutation(arr) {
+//   //using .map() to lowercase all letters in the array
+//   //using .split() to create arrays of each letter within each array
+//   let string = arr.map(i => i.toLowerCase().split(''))
+//   // return string[0].indexOf('k')
+//   for (var i = 0; i < string[1].length; i++) {
+//     if (string[0].indexOf(string[1][i]) === -1) {
+//          return false
+//     }
+//   }
+//   return true
+// }
+
+// optimized solution
 function mutation(arr) {
-  //using .map() to lowercase all letters in the array
-  //using .split() to create arrays of each letter within each array
   let string = arr.map(i => i.toLowerCase().split(''))
-  // return string[0].indexOf('k')
-  for (var i = 0; i < string[1].length; i++) {
-    if (string[0].indexOf(string[1][i]) === -1) {
-         return false
-    }
-}
-return true
+
+  return string[1]
+  .every(function(i) {
+     return string[0].indexOf(i) != -1;
+  });
 }
 
 
