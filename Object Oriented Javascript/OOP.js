deedeb87138c244599c45dff689edef4cd51375e
bijjr <billiejefferys@gmail.com>
2018-09-12 23:20:39 -0400

    .hasOwnProperty, Prototype objects

--- Object Oriented Javascript/OOP.js
+++ Object Oriented Javascript/OOP.js
@@ -38,3 +38,46 @@ function Dog(name, color) {
 }
 
 let terrier = new Dog('Robyn','Red')
+
+
+//Verify an objects constructor with intstanceof
+
+function House(numBedrooms) {
+  this.numBedrooms = numBedrooms;
+}
+
+let myHouse = new House(2);
+
+myHouse instanceof House;
+
+//Add the own properties of canary to the array ownProps
+
+function Bird(name) {
+  this.name = name;
+  this.numLegs = 2;
+}
+
+let canary = new Bird("Tweety");
+let ownProps = [];
+
+for (let property in canary) {
+  if (canary.hasOwnProperty(property)) {
+    ownProps.push(property);
+  }
+}
+
+console.log(ownProps);
+
+//Prototype properties to reduce duplicate code
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype.numLegs = 2;
+
+let beagle = new Dog("Snoopy");
+
+//
+
+//
