a80ad2a6bd761a193f1ba686eafc46532c42543f
bijjr <billiejefferys@gmail.com>
2018-09-17 23:27:04 -0400

    Return a Sorted Array Without Changing the Original Array

--- Function Programming/functional.js
+++ Function Programming/functional.js
@@ -617,3 +617,12 @@ console.log(averageRating);
 // //     },0)
 //
 // console.log(averageRating);
+
+var globalArray = [5, 6, 3, 2, 9];
+function nonMutatingSort(arr) {
+  // Add your code below this line
+  return arr.sort();
+
+  // Add your code above this line
+}
+console.log(nonMutatingSort(globalArray));
