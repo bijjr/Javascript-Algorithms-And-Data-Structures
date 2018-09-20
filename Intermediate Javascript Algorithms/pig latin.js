ecf51025aee798a464306557ba4cd4cf1d5ae173
bijjr <billiejefferys@gmail.com>
2018-09-20 01:11:22 -0400

    Adding Pig latin exercise

---
+++ Intermediate Javascript Algorithms/pig latin.js
@@ -0,0 +1,30 @@
+// Translate the provided string to pig latin.
+//
+// Pig Latin takes the first consonant (or consonant cluster)
+// of an English word, moves it to the end of the word and suffixes an "ay".
+//
+// If a word begins with a vowel you just add "way" to the end.
+//
+// Input strings are guaranteed to be English words in all lowercase.
+
+
+
+function translatePigLatin(str) {
+  return str;
+}
+
+console.log(translatePigLatin("consonant");
+
+console.log(translatePigLatin("california") );
+// should return "aliforniacay".
+console.log(translatePigLatin("paragraphs") );
+// should return "aragraphspay".
+console.log(translatePigLatin("glove"));
+ // should return "oveglay".
+console.log(translatePigLatin("algorithm") );
+// should return "algorithmway".
+console.log(translatePigLatin("eight"));
+// should return "eightway".
+
+// Should handle words where the first vowel comes in the end of the word.
+// Should handle words without vowels.
