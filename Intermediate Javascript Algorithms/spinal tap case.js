3a7c25dbf5becc043801d8db53d91d12eae046a0
bijjr <billiejefferys@gmail.com>
2018-09-20 00:15:20 -0400

    Spinal Tap solution

--- Intermediate Javascript Algorithms/spinal tap case.js
+++ Intermediate Javascript Algorithms/spinal tap case.js
@@ -1,9 +1,22 @@
 // Convert a string to spinal case. Spinal case is all-lowercase-words-joined-by-dashes.
 
 function spinalCase(str) {
-  // "It's such a fine line between stupid, and clever."
-  // --David St. Hubbins
-  return str;
+  // regex for whitespace and underscores in the entire string
+  let regex = /\s+|_+/g;
+  //replaces any instances of camel cases throughout the string with a space between them
+  str = str.replace(/([a-z])([A-Z])/g, '$1 $2')
+  //replaces and spaces or underscores with '-' and lowercases the string
+  return str.replace(regex, "-").toLowerCase();
 }
 
-spinalCase('This Is Spinal Tap');
+
+console.log(spinalCase("This Is Spinal Tap"));
+// should return "this-is-spinal-tap".
+console.log(spinalCase("thisIsSpinalTap"));
+// should return "this-is-spinal-tap".
+console.log(spinalCase("The_Andy_Griffith_Show"));
+// should return "the-andy-griffith-show".
+console.log(spinalCase("Teletubbies say Eh-oh"));
+// should return "teletubbies-say-eh-oh".
+console.log(spinalCase("AllThe-small Things"));
+// should return "all-the-small-things".
