a42784a22171cba5b1ecb6acd04def4e8b354476
bijjr <billiejefferys@gmail.com>
2018-09-22 19:39:20 -0400

    Psuedo code

--- Intermediate Javascript Algorithms/pig latin.js
+++ Intermediate Javascript Algorithms/pig latin.js
@@ -2,18 +2,32 @@
 //
 // Pig Latin takes the first consonant (or consonant cluster)
 // of an English word, moves it to the end of the word and suffixes an "ay".
-//
 // If a word begins with a vowel you just add "way" to the end.
-//
 // Input strings are guaranteed to be English words in all lowercase.
-
+// Should handle words where the first vowel comes in the end of the word.
+// Should handle words without vowels
 
 
 function translatePigLatin(str) {
-  return str;
+  let vowels = ['a','e','i','o','u']
+
+  if constant or splice until vowel?
+    add to end of word/array
+
+  if first letter = vowel
+    concat way to the end
+
+   // str.split('').map((letter) => {
+   //    vowels.foreach((item) => {
+   //      return letter != item
+   //    })
+   // })
+
+   return str
 }
 
-console.log(translatePigLatin("consonant");
+console.log('\n');
+console.log(translatePigLatin("consonant"));
 
 console.log(translatePigLatin("california") );
 // should return "aliforniacay".
@@ -25,6 +39,4 @@ console.log(translatePigLatin("algorithm") );
 // should return "algorithmway".
 console.log(translatePigLatin("eight"));
 // should return "eightway".
-
-// Should handle words where the first vowel comes in the end of the word.
-// Should handle words without vowels.
+console.log('\n');
